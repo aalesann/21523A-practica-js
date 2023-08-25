16788bfb15622b5761d64d2a353e463e5dc72250
aalesann <alejandrosanabria74@gmail.com>
2023-08-25 16:33:35 -0300

    Práctica tipos de datos en JavaScript

---
+++ README.md
@@ -0,0 +1,5 @@
+# Argentina Programa 4.0
+
+## Práctica de JavaScript
+
+![Javascript](https://midu.dev/images/wallpapers/una-taza-de-javascript.png)
---
+++ tipos-de-datos.js
@@ -0,0 +1,78 @@
+
+
+//! PRIMITIVOS
+
+// let a, b, c;
+// let nombrePersona, apellidoPersona, ciudad;
+
+// nombrePersona = 'John';
+// apellidoPersona = 'Doe';
+// ciudad = "Buenos Aires";
+// const premisa = true;
+// var edad = 39;
+// let vacio = null;
+// console.log(vacio);
+
+//! COMPLEJOS - ESTRUCTURADOS
+
+//! Objetos literales
+// let producto = {
+//     marca: "X",
+//     precio: 100,
+// }
+
+// console.log(producto.precio + 22)
+
+// ! Objeto Literal
+// let persona = {
+//     nombre: "John",
+//     apellido: "Doe",
+//     edad: "23",
+//     dni: 34345222,
+//     correr: function(){
+//         console.log('Estoy corriendo')
+//     }
+// }
+// console.log(persona.correr())
+
+//! Desestructuración de objetos literales
+// let persona = {
+//     nombre: "John",
+//     apellido: "Doe",
+//     edad: "23",
+//     dni: 34345222,
+//     correr: function(){
+//         console.log('Estoy corriendo')
+//     }
+// }
+// persona = "Jane"
+// console.log(persona)
+
+
+//! Arreglos
+// let lista0 = []
+
+// let listaNueva = [1, 2, 3, 4, 5]
+
+// let lista = ["Ale", 2, { nombre: "John", apellido: "Doe" }, true]
+
+//! Desestructuración de arreglos
+// const [, numero , alguien, valor ] = lista;
+
+// console.log(lista[0])
+// console.log(lista[1])
+// console.log(lista[2].nombre)
+// console.log(lista[2].apellido)
+// console.log(lista[3])
+
+// console.log(nombre)
+// console.log(numero)
+// console.log(alguien.nombre)
+// console.log(valor)
+
+
+
+
+
+
+
